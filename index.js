268f184e755c4bd1b3e158b5028666e6437ef304
lucky <takuto.yoshikai@gmail.com>
2021-01-25 13:49:05 +0900

    secret

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+let step = 0;
+
+function secret() {
+  const i = Math.floor(Math.random() * 3);
+  let text = "You are ";
+  for (let j = 0; j <= i; j++) {
+    text += "SUPER ";
+  }
+  text += "LUCKY!!!";
+  alert(text);
+}
+
 window.onload = function() {
-  
+  document.getElementById("ch1").onclick = function() {
+    if (step === 0) {
+      step = 1;
+    } else {
+      step = 0;
+    }
+  }
+  document.getElementById("ch2").onclick = function() {
+    if (step === 1) {
+      step = 2;
+    } else {
+      step = 0;
+    }
+  }
+  document.getElementById("ch3").onclick = function() {
+    if (step === 2) {
+      secret();
+    }
+    step = 0;
+  }
 }
