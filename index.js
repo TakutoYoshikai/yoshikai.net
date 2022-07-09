70c56a975431a94805d3d4db82fcb5407c9ba3c7
lucks <takuto.yoshikai@gmail.com>
2022-07-09 19:37:39 +0700

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+  document.getElementById("chloe").onclick = function () {
+    document.getElementById("name").innerHTML = "I love my girlfriend Saung.";
+    document.getElementById("job").innerHTML = "I vow eternal love.";
+  }
+}
