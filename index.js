155dede6003273e3da4a86b62e3c1775e5656d64
lucky <takuto.yoshikai@gmail.com>
2022-08-11 14:31:06 +0900

    saung

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 window.onload = function () {
   document.getElementById("chloe").onclick = function () {
-    document.getElementById("name").innerHTML = "I love my girlfriend Saung.";
-    document.getElementById("job").innerHTML = "I vow eternal love.";
+    document.getElementById("name").innerHTML = "I love my fiance Saung Zarchi Hnin. I vow eternal love.";
+    document.getElementById("job").innerHTML = "";
   }
+  let title = document.title;
+  setInterval(() => {
+    const now = new Date();
+    if (now.getHours() == 16 && now.getMinutes() == 54) {
+      document.title = "I love Saung💖 Marry me😍";
+    } else {
+      document.title = title;
+    }
+  }, 1000);
 }
